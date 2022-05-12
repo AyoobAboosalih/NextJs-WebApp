3117c9e8ff14846f9ad6bf1ce92ace3f5672f573
Ayoob Aboosalih <ayoob.2018324@iit.ac.lk>
2022-05-12 21:29:04 +0530

    Linking Pages

--- nextjs-web-app/pages/index.js
+++ nextjs-web-app/pages/index.js
@@ -3,6 +3,7 @@ import Image from "next/image";
 import Footer from "../comps/Footer";
 import Navbar from "../comps/Navbar";
 import styles from "../styles/Home.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -10,6 +11,9 @@ export default function Home() {
       <Navbar />
       <h1>Home Page</h1>
       <p>Content</p>
+      <Link href="/ninjas">
+        <a>See Ninja Listing</a>
+      </Link>
       <Footer />
     </div>
   );
